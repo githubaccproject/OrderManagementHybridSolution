6e3737c63f74cc99710bc192a144fff7b68b5ee8
Patil, Santhosh <SPATIL25@volvocars.com>
2025-11-22 08:42:48 +0530

    updated

--- src/OrderManagement.Database/StoredProcedures/GetCustomerOrders.sql
+++ src/OrderManagement.Database/StoredProcedures/GetCustomerOrders.sql
@@ -1,12 +1,11 @@
-
 CREATE PROCEDURE [dbo].[GetCustomerOrders]
-(
     @CustomerId INT
-)
 AS
 BEGIN
-    SET NOCOUNT ON;
-    SELECT o.OrderId, o.OrderDate, o.Amount
+    SELECT 
+        o.OrderId,
+        o.OrderDate,
+        o.Amount
     FROM Orders o
     WHERE o.CustomerId = @CustomerId;
 END
